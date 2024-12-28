0b206f8ff423dc4aa07978e271fd597d8b4f5f95
fosseddy <markfosseddy@gmail.com>
2024-12-29 04:40:55 +0500

    version 1.0.0

--- nvim/lua/html-tags.lua
+++ nvim/lua/html-tags.lua
@@ -1,212 +1,243 @@
+--grammar:
+--    primary = tag (sequence | increase_nesting | decrease_nesting)?
+
+--    sequence = "," primary
+--    increase_nesting = ">" primary
+--    decrease_nesting = "<"+ primary
+
+--    tag = any char except "," ">" "<"
+
 local M = {}
 
-local CMD_IDENT = "ht:"
+local CMD_TRIGGER = ":ht"
 
 local SINGLE_TAGS = {
-    "input", "img", "br", "hr", "meta", "link", "area", "base", "wbr", "col",
-    "embed", "param", "source", "track"
+    input  = 1, img   = 1, br  = 1, hr  = 1, meta  = 1, link  = 1,
+	area   = 1, base  = 1, wbr = 1, col = 1, embed = 1, param = 1,
+	source = 1, track = 1
 }
 
-local function tobytes(s)
-    local b = {}
-    for i = 1, #s do
-        b[i] = string.byte(s, i)
+local function get_utf8_char(str, i)
+    local rune = vim.fn.strgetchar(str, i - 1)
+    return vim.fn.nr2char(rune)
+end
+
+local function is_single_tag(tag)
+    return SINGLE_TAGS[tag] ~= nil
+end
+
+local function is_custom_tag(tag)
+    local ch = get_utf8_char(tag, 1)
+    return ch == vim.fn.toupper(ch)
+end
+
+local function report_error(state, fmt, ...)
+    local msg = string.format(fmt, ...)
+
+    state.error = string.format("%d: %s", state.cursor, msg)
+    state.has_error = true
+end
+
+local function is_end_of_input(state)
+    return state.cursor > #state.input
+end
+
+local function peek(state)
+    if is_end_of_input(state) then
+        return ""
     end
-    return b
+
+    local current = string.sub(state.input, state.cursor)
+    return get_utf8_char(current, 1)
 end
 
-local function cursor_position()
-    local pos = vim.api.nvim_win_get_cursor(0)
-    return pos[1] - 1, pos[2]
+local function match(state, ch)
+    return peek(state) == ch
 end
 
-local function is_uppercase(ch)
-    return ch >= string.byte("A") and ch <= string.byte("Z")
+local function advance(state)
+    if is_end_of_input(state) then
+        error("tried to advance after the end of input", 2)
+    end
+    state.cursor = state.cursor + 1
 end
 
-local function is_single(tag)
-    for i = 1, #SINGLE_TAGS do
-        if tag == SINGLE_TAGS[i] then
-            return true
+local function insert_output(state, tag)
+    local whitespace = "\t"
+    local level = state.indent
+
+    if vim.bo.expandtab then
+        whitespace = " "
+        level = level * vim.fn.shiftwidth()
+    end
+
+    table.insert(state.output, string.rep(whitespace, level)..tag)
+end
+
+local function parse_pattern(state, pattern)
+    local start, _end = string.find(state.input, pattern, state.cursor)
+
+    if start == nil then
+        return false, ""
+    end
+
+    state.cursor = _end + 1
+    return true, string.sub(state.input, start, _end)
+end
+
+local function parse_indent(state)
+    local _, whitespace = parse_pattern(state, "^%s*") -- starts with 0 or more whitespaces
+
+    -- NOTE(art), 25.12.24: we are assuming that indentation is not a mix of tabs and spaces
+    local indent = string.len(whitespace)
+    if vim.bo.expandtab then
+        indent = math.floor(indent / vim.fn.shiftwidth())
+    end
+
+    state.indent = indent
+end
+
+local compile_primary
+
+local function compile_decrease_nesting(state)
+    if state.has_error then
+        return
+    end
+
+    while match(state, "<") do
+        local tag = table.remove(state.parents)
+        if tag == nil then
+            report_error(state, "not enough parents to decrease nesting")
+            return
+        end
+
+        advance(state)
+
+        state.indent = state.indent - 1
+        insert_output(state, "</"..tag..">")
+    end
+
+    compile_primary(state)
+end
+
+local function compile_increase_nesting(state)
+    if state.has_error then
+        return
+    end
+
+    advance(state)
+    state.indent = state.indent + 1
+
+    compile_primary(state)
+
+    local tag = table.remove(state.parents)
+    if tag ~= nil then
+        state.indent = state.indent - 1
+        insert_output(state, "</"..tag..">")
+    end
+end
+
+local function compile_sequence(state)
+    if state.has_error then
+        return
+    end
+
+    advance(state)
+    compile_primary(state)
+end
+
+local function compile_tag(state)
+    if state.has_error then
+        return
+    end
+
+    local found, tag = parse_pattern(state, "^[^,><]+") -- starts with 1 or more of any, except ,><
+    if not found then
+        report_error(state, "expected tag, but got '%s'", peek(state))
+        return
+    end
+
+    local output = ""
+
+    if match(state, ">") then
+        table.insert(state.parents, tag)
+        output = "<"..tag..">"
+    else
+        if is_single_tag(tag) then
+            output = "<"..tag..">"
+        elseif is_custom_tag(tag) then
+            output = "<"..tag.." />"
+        else
+            output = "<"..tag.."></"..tag..">"
         end
     end
-    return false
+
+    insert_output(state, output)
 end
 
-local function Parser(line, offset)
-    return {
-        line = line,
-        offset = offset,
+function compile_primary(state)
+    if state.has_error then
+        return
+    end
+
+    compile_tag(state)
+
+    if is_end_of_input(state) then
+        return
+    elseif match(state, ",") then
+        compile_sequence(state)
+    elseif match(state, ">") then
+        compile_increase_nesting(state)
+    elseif match(state, "<") then
+        compile_decrease_nesting(state)
+    else
+        report_error(state, "unknown symbol '%s'", peek(state))
+        return
+    end
+end
+
+function M.compile_line()
+    local cursor_row, cursor_col = unpack(vim.api.nvim_win_get_cursor(0))
+    local state = {
+        input = vim.b.html_tags_input,
+        cursor = 1,
         indent = 0,
+        error = "",
+        has_error = false,
         parents = {},
         output = {}
     }
-end
 
-local function parser_error(parser, msg)
-    local fmt = string.format("%d : %s", parser.offset, msg)
-    error(fmt, 0)
-end
+    parse_indent(state)
+    compile_primary(state)
 
-local function peek(parser)
-    return parser.line[parser.offset]
-end
-
-local function end_of_line(parser)
-    return peek(parser) == nil
-end
-
-local function match(parser, char)
-    return peek(parser) == string.byte(char)
-end
-
-local function advance(parser)
-    if end_of_line(parser) then
-        return 0
+    if state.has_error then
+        vim.cmd.stopinsert()
+        vim.schedule(function()
+            vim.api.nvim_win_set_cursor(0, {cursor_row, state.cursor - 1})
+            vim.api.nvim_err_writeln(string.format("html-tags:%s", state.error))
+        end)
+        return
     end
 
-    local ch = peek(parser)
-    parser.offset = parser.offset + 1
-    return ch
+    local row = cursor_row - 1
+    local output = state.output
+
+    vim.api.nvim_buf_set_text(0, row, 0, row, cursor_col, output)
+    vim.api.nvim_win_set_cursor(0, {row + #output, #output[#output]})
 end
 
-local function add_indent(parser, output)
-    local indent = string.rep("\t", parser.indent)
-    return indent .. output
-end
-
-local function parse_tag(parser)
-    local tag_bytes = {}
-    while not end_of_line(parser) and
-          not match(parser, ",") and
-          not match(parser, ">") and
-          not match(parser, "<")
-    do
-        table.insert(tag_bytes, advance(parser))
-    end
-
-    local tag = string.char(unpack(tag_bytes))
-    if tag == "" then
-        parser_error(parser, "no tag")
-    end
-
-    local output = "<" .. tag
-
-    if match(parser, ">") then
-        table.insert(parser.parents, tag)
-        output = output .. ">"
-    else
-        if is_uppercase(tag_bytes[1]) then
-            output = output .. " />"
-        elseif is_single(tag) then
-            output = output .. ">"
-        else
-            output = output .. "></" .. tag .. ">"
-        end
-    end
-
-    if #parser.output > 0 then
-        output = add_indent(parser, output)
-    end
-
-    table.insert(parser.output, output)
-end
-
-local function parse_seq(parser)
-    parse_tag(parser)
-    while match(parser, ",") do
-        advance(parser)
-        parse_tag(parser)
-    end
-end
-
-local function parse_nest(parser)
-    advance(parser)
-    parser.indent = parser.indent + 1
-    parse_seq(parser)
-end
-
-local function parse_unnest(parser)
-    while match(parser, "<") do
-        advance(parser)
-
-        local tag = table.remove(parser.parents)
-        if tag == nil then
-            parser_error(parser, "nest underflow")
-        end
-
-        parser.indent = parser.indent - 1
-
-        local output = add_indent(parser, "</" .. tag .. ">")
-        table.insert(parser.output, output)
-    end
-
-    parse_seq(parser)
-end
-
-local function parse_cmd(parser)
-    parse_seq(parser)
-
-    while not end_of_line(parser) do
-        if match(parser, ">") then
-            parse_nest(parser)
-        elseif match(parser, "<") then
-            parse_unnest(parser)
-        else
-            parser_error(parser, "unexpected character")
-            break
-        end
-    end
-
-    while #parser.parents > 0 do
-        local tag = table.remove(parser.parents)
-        if tag == nil then
-            parser_error(parser, "nest underflow")
-        end
-
-        parser.indent = parser.indent - 1
-
-        local output = add_indent(parser, "</" .. tag .. ">")
-        table.insert(parser.output, output)
-    end
-end
-
---grammar:
---  tag = any char except ',><'
---  seq = tag (',' tag)+?
---  nest = '>' seq
---  unnest = '<'+ seq
---  cmd = seq (nest | unnest)+?
-
-function M.compile()
+function M.line_has_trigger()
+    local col = vim.api.nvim_win_get_cursor(0)[2]
     local line = vim.api.nvim_get_current_line()
-    local cmd_start, cmd_end = string.find(line, CMD_IDENT, 1, true)
+    local input = string.sub(line, 1, col)
 
-    if cmd_start == nil then
+    if string.sub(input, -#CMD_TRIGGER) ~= CMD_TRIGGER then
         return false
     end
 
-    if cmd_start > 1 then
-        local ch = string.byte(line, cmd_start - 1)
-        if ch ~= string.byte(" ") and ch ~= string.byte("\t") then
-            return false
-        end
-    end
-
-    local parser = Parser(tobytes(line), cmd_end + 1)
-
-    local ok, err = pcall(parse_cmd, parser)
-    if not ok then
-        vim.api.nvim_err_writeln(string.format("html-tags: %s", err))
-        return false
-    end
-
-    cursor_row = cursor_position()
-    vim.api.nvim_buf_set_text(0, cursor_row, cmd_start - 1, cursor_row, #line, parser.output)
-
+    vim.b.html_tags_input = string.sub(input, 1, #input - #CMD_TRIGGER)
     return true
 end
-
---ht:html>head>meta,title<body>header>nav>List>ListItem<<<main>h1,input,section<script
 
 return M
