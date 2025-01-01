6a6f3d648f1e9fd0d15715b9de09106f5259ed1a
fosseddy <markfosseddy@gmail.com>
2025-01-01 22:41:09 +0500

    html-tags v1.0.0

--- nvim/lua/html-tags.lua
+++ nvim/lua/html-tags.lua
@@ -63,15 +63,8 @@ local function advance(state)
 end
 
 local function insert_output(state, tag)
-    local whitespace = "\t"
-    local level = state.indent
-
-    if vim.bo.expandtab then
-        whitespace = " "
-        level = level * vim.fn.shiftwidth()
-    end
-
-    table.insert(state.output, string.rep(whitespace, level)..tag)
+    local indent_len = state.indent.level * state.indent.multiplier
+    table.insert(state.output, string.rep(state.indent.symbol, indent_len)..tag)
 end
 
 local function parse_pattern(state, pattern)
@@ -87,14 +80,9 @@ end
 
 local function parse_indent(state)
     local _, whitespace = parse_pattern(state, "^%s*") -- starts with 0 or more whitespaces
-
-    -- NOTE(art), 25.12.24: we are assuming that indentation is not a mix of tabs and spaces
     local indent = string.len(whitespace)
-    if vim.bo.expandtab then
-        indent = math.floor(indent / vim.fn.shiftwidth())
-    end
 
-    state.indent = indent
+    state.indent.level = math.floor(indent / state.indent.multiplier)
 end
 
 local compile_primary
@@ -113,7 +101,7 @@ local function compile_decrease_nesting(state)
 
         advance(state)
 
-        state.indent = state.indent - 1
+        state.indent.level = state.indent.level - 1
         insert_output(state, "</"..tag..">")
     end
 
@@ -126,13 +114,13 @@ local function compile_increase_nesting(state)
     end
 
     advance(state)
-    state.indent = state.indent + 1
+    state.indent.level = state.indent.level + 1
 
     compile_primary(state)
 
     local tag = table.remove(state.parents)
     if tag ~= nil then
-        state.indent = state.indent - 1
+        state.indent.level = state.indent.level - 1
         insert_output(state, "</"..tag..">")
     end
 end
@@ -191,31 +179,49 @@ function compile_primary(state)
     elseif match(state, "<") then
         compile_decrease_nesting(state)
     else
-        report_error(state, "unknown symbol '%s'", peek(state))
+        error("unknown symbol '"..peek(state).."'")
         return
     end
 end
 
-function M.compile_line()
-    local cursor_row, cursor_col = unpack(vim.api.nvim_win_get_cursor(0))
-    local state = {
-        input = vim.b.html_tags_input,
+function compile(state)
+    parse_indent(state)
+    compile_primary(state)
+end
+
+function State(input)
+    -- NOTE(art), 25.12.24: we are assuming that indentation is not a mix of tabs and spaces
+    local indent_symbol, indent_multiplier = "\t", 1
+    if vim.bo.expandtab then
+        indent_symbol, indent_multiplier = " ", vim.fn.shiftwidth()
+    end
+
+    return {
+        input = input,
         cursor = 1,
-        indent = 0,
+        indent = {
+            level = 0,
+            symbol = indent_symbol,
+            multiplier = indent_multiplier
+        },
         error = "",
         has_error = false,
         parents = {},
         output = {}
     }
+end
 
-    parse_indent(state)
-    compile_primary(state)
+function M.compile_line()
+    local cursor_row, cursor_col = unpack(vim.api.nvim_win_get_cursor(0))
+    local state = State(vim.b.html_tags_input)
+
+    compile(state)
 
     if state.has_error then
         vim.cmd.stopinsert()
         vim.schedule(function()
             vim.api.nvim_win_set_cursor(0, {cursor_row, state.cursor - 1})
-            vim.api.nvim_err_writeln(string.format("html-tags:%s", state.error))
+            vim.api.nvim_err_writeln("html-tags:"..state.error)
         end)
         return
     end
@@ -241,3 +247,101 @@ function M.line_has_trigger()
 end
 
 return M
+--[[
+
+local function print_err(description, label, got, want, state)
+    vim.print(description, "", "got "..label, got, "", "but want", want, "", state)
+end
+
+local function check_output(got, want)
+    if #got ~= #want then
+        return false
+    end
+    for i = 1, #got do
+        if got[i] ~= want[i] then
+            return false
+        end
+    end
+    return true
+end
+
+local cases_success = {
+    ["tag"] = {input = "div", want = {"<div></div>"}},
+    ["tag utf-8"] = {input = "див", want = {"<див></див>"}},
+    ["single tag"] = {input = "input", want = {"<input>"}},
+    ["custom tag"] = {input = "List", want = {"<List />"}},
+    ["custom tag utf-8"] = {input = "Лист", want = {"<Лист />"}},
+
+    ["sequence"] = {input = "a,b", want = {"<a></a>", "<b></b>"}},
+    ["sequence multiple"] = {input = "a,b,c", want = {"<a></a>", "<b></b>", "<c></c>"}},
+    ["sequence different types of tags"] = {input = "a,br,C", want = {"<a></a>", "<br>", "<C />"}},
+
+    ["increase nesting"] = {input = "a>b", want = {"<a>", "    <b></b>", "</a>"}},
+    ["increase nesting multiple"] = {input = "a>b>c", want = {"<a>", "    <b>", "        <c></c>", "    </b>", "</a>"}},
+    ["increase nesting single tag"] = {input = "meta>a", want = {"<meta>", "    <a></a>", "</meta>"}},
+    ["increase nesting custom tag"] = {input = "List>ListItem", want = {"<List>", "    <ListItem />", "</List>"}},
+    ["increase nesting sequence"] = {input = "a>b,c", want = {"<a>", "    <b></b>", "    <c></c>", "</a>"}},
+
+    ["decrease nesting"] = {input = "a>b<c", want = {"<a>", "    <b></b>", "</a>", "<c></c>"}},
+    ["decrease nesting sequence"] = {input = "a>b<c,d", want = {"<a>", "    <b></b>", "</a>", "<c></c>", "<d></d>"}},
+    ["decrease nesting 2"] = {input = "a>b>c<d", want = {"<a>", "    <b>", "        <c></c>", "    </b>", "    <d></d>", "</a>"}},
+    ["decrease nesting 3"] = {input = "a>b>c<<d", want = {"<a>", "    <b>", "        <c></c>", "    </b>", "</a>", "<d></d>"}},
+
+    ["with indent"] = {input = "    div", want = {"    <div></div>"}},
+    ["with indent 2"] = {input = "   div", want = {"<div></div>"}},
+    ["with indent 3"] = {input = "  div", want = {"<div></div>"}},
+    ["with indent 4"] = {input = " div", want = {"<div></div>"}},
+
+    ["with tab indent"] = {input = "\tdiv", use_tab = true, want = {"\t<div></div>"}},
+    ["with tab indent 2"] = {input = "\t\tdiv", use_tab = true, want = {"\t\t<div></div>"}},
+    ["with tab indent 3"] = {input = "a>b>c<d", use_tab = true, want = {"<a>", "\t<b>", "\t\t<c></c>", "\t</b>", "\t<d></d>", "</a>"}},
+}
+
+for description, case in pairs(cases_success) do
+    local state = State(case.input)
+    if case.use_tab then
+        state.indent.symbol = "\t"
+        state.indent.multiplier = 1
+    end
+
+    compile(state)
+
+    if state.has_error then
+        print_err(description, "has_error", state.has_error, false, state)
+        break
+    end
+
+    if not check_output(state.output, case.want) then
+        print_err(description, "output", state.output, case.want, state)
+        break
+    end
+end
+
+local cases_fail = {
+    ["no tag"] = {input = "", want = 1},
+    ["no tag 2"] = {input = ",", want = 1},
+    ["no tag 3"] = {input = ">", want = 1},
+    ["no tag 4"] = {input = "<", want = 1},
+    ["no tag 5"] = {input = "div>", want = 5},
+    ["no tag 6"] = {input = "div,", want = 5},
+    ["not enough parents"] = {input = "div<", want = 4},
+    ["not enough parents 2"] = {input = "div>div<<", want = 9},
+    ["not enough parents 3"] = {input = "a>b>c<d<e<", want = 10},
+}
+
+for description, case in pairs(cases_fail) do
+    local state = State(case.input)
+    compile(state)
+
+    if not state.has_error then
+        print_err(description, "has_error", state.has_error, true, state)
+        break
+    end
+
+    if state.cursor ~= case.want then
+        print_err(description, "cursor", state.cursor, case.want, state)
+        break
+    end
+end
+
+--]]
